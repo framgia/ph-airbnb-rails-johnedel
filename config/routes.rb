b97ebe8b2ca6357d572a42c31b4e198e6ba43a1a
Johnedel S. Mapa <34313943+johnedelmapa@users.noreply.github.com>
2019-08-09 15:32:27 +0800

    Task basic authentication (#2)
    * Basic Authentication
    
    * Fix comments (rm font-awesome )

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, path: '', path_names: {sign_in: 'login', sign_up: 'registration', sign_out: 'logout'}
+  root 'static_pages#home'
 end
