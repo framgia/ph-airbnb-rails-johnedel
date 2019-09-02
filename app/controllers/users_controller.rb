a86ea91a445b67b054b477ddfb12654f5924cdaf
johnedelmapa <johnedel.mapa@sun-asterisk.com>
2019-09-02 18:16:10 +0800

    Add average reviews to rooms, remove reviews from user itself, and add links for user trips and reservation.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @rooms = Room.where(user_id: @user)
     @guests = Review.where(host_id: @user) 
-    @hosts = Review.where(guest_id: @user)
+    @hosts = Review.where(guest_id: !@user)
     @room = Room.find(params[:id])
     @reviews = Review.where(room_id: @room)
   end
