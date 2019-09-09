46001267022897272797eb3fda906ff1b0c9c5b7
johnedelmapa <johnedel.mapa@sun-asterisk.com>
2019-09-09 17:42:53 +0800

    Polish homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     @search = Room.ransack(params[:q])
     @rooms = @search.result
+    @allRooms = Room.where(is_active: true).limit(3)
   end
 
   def map
